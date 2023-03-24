381e9805dfa03cb5ab81b842676b327bfd5a0a69
Kia <kia.hakala@outlook.com>
2023-03-24 14:19:33 +0200

    uibuild

--- advanced-redux-backend/index.js
+++ advanced-redux-backend/index.js
@@ -1,75 +1,100 @@
+require('dotenv').config()
 const express = require('express')
 const { v4: generateId } = require('uuid')
 const cors = require('cors')
 const app = express()
 
+const Product = require('./models/product')
+  
 // json parser
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-let DUMMY_PRODUCTS = [
-	{
-		id: 'p1',
-		price: 6,
-		title: 'The first product',
-		description: 'The first product in this store'
-	},
-	{
-		id: 'p2',
-		price: 8,
-		title: 'The 2nd product',
-		description: 'The 2nd product in this store'
-	}
-]
-
-app.get('/', (req, res) => {
-  	res.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (req, res) => {
+//   	res.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/products', (req, res) => {
-  	res.json(DUMMY_PRODUCTS)
+	Product.find({}).then(products => {
+		res.json(products)
+	})
 })
 
-app.get('/api/products/:id', (req, res) => {
-	const id = req.params.id
-	const product = DUMMY_PRODUCTS.find(product => product.id === id)
-	
-	if (product) {
-		res.json(product)
-	} else {
-		res.status(404).end()
-	}
+app.get('/api/products/:id', (req, res, next) => {
+	Product.findById(req.params.id)
+	.then(product => {
+		if (product) {
+			res.json(product)
+		} else {
+			res.status(404).end()
+		}
+	})
+	.catch(error => next(error))
 })
 
-app.delete('/api/products/:id', (req, res) => {
-	const id = req.params.id
-	DUMMY_PRODUCTS = DUMMY_PRODUCTS.filter(product => product.id !== id)
-
-	res.status(204).end()
+app.delete('/api/products/:id', (req, res, next) => {
+	Product.findByIdAndRemove(req.params.id)
+	.then(result => {
+		res.status(204).end()
+	})
+	.catch(error => next(error))
 })
 
 app.post('/api/products', (req, res) => {
 	const body = req.body
-
-	if (!body.title) {
-		return response.status(400).json({ 
-		  	error: 'title missing' 
-		})
-	  }
-	
-	const product = {
+  
+	if (body.title === undefined) {
+	  	return res.status(400).json({ error: 'Title missing' })
+	}
+  
+	const product = new Product({
 		id: generateId(),
 		price: body.price,
 		title: body.title,
 		description: body.description
-	}
-
-	DUMMY_PRODUCTS = DUMMY_PRODUCTS.concat(product)
-	res.json(product)
+	})
+  
+	product.save().then(savedProduct => {
+	  	res.json(savedProduct)
+	})
 })
 
-const PORT = process.env.PORT || 8001
+app.put('/api/products/:id', (req, res, next) => {
+	const body = req.body
+  
+	const product = {
+	  title: body.title,
+	  price: body.price,
+	  description: body.description
+	}
+  
+	Product.findByIdAndUpdate(req.params.id, product, { new: true })
+	  .then(updatedProduct => {
+		res.json(updatedProduct)
+	  })
+	  .catch(error => next(error))
+  })
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message)
+  
+	if (error.name === 'CastError') {
+	  	return res.status(400).send({ error: 'malformatted id' })
+	}
+  
+	next(error)
+}
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// error handling after registering other middlewares!
+app.use(errorHandler)
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  	console.log(`Server running on port ${PORT}`)
 })
